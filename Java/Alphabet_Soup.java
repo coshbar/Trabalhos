fa59cda5189dc71e7e7cb8b5cabfa7fb92aa103a
coshbar <43322369+coshbar@users.noreply.github.com>
2021-04-03 20:59:37 -0300

    Update and rename Alphabet Soup to Alphabet_Soup.java

--- Java/Alphabet_Soup.java
+++ Java/Alphabet_Soup.java
@@ -1,5 +1,7 @@
-#Have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo).
-#Assume numbers and punctuation symbols will not be included in the string.
+/*
+* Have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo).
+* Assume numbers and punctuation symbols will not be included in the string.
+*/
 
 import java.util.*; import java.io.*;
 
@@ -9,12 +11,10 @@ class Main {
     char[] chars = str.toCharArray();
     Arrays.sort(chars);
     str = new String(chars);
-    // code goes here  
     return str;
   }
 
   public static void main (String[] args) {  
-    // keep this function call here     
     Scanner s = new Scanner(System.in);
     System.out.print(StringChallenge(s.nextLine())); 
   }
