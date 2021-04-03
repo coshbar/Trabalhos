f806ebef59e65c006c417b49344edbd04e8700d3
coshbar <43322369+coshbar@users.noreply.github.com>
2021-04-03 20:51:12 -0300

    Add files via upload

---
+++ C#/2/Notebook.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Notebook
+    {
+        static List<Employee> _note = new();
+        public static void Add_employee(Employee _employee) => _note.Add(_employee);
+        public static List<Employee> Get_employee() => _note;
+        public static Employee Search_employee(string _cpf)
+        {
+            foreach (Employee _temp in _note)
+                if (_temp.Cpf.Equals(_cpf))
+                    return _temp;
+
+            return null;
+        }
+    }
+}
---
+++ C#/2/Sheetbook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Sheetbook
+    {
+        static List<Sheet> _sheet = new();
+        public static void Add_sheet(Sheet _sheetbook) => _sheet.Add(_sheetbook);
+        public static List<Sheet> GetSheet() => _sheet;
+        public static Sheet Search_duplicate(string _cpf, int _month, int _year)
+        {
+            foreach (Sheet _temp in _sheet)
+                if (_temp.Cpf.Equals(_cpf) && _temp.Month.Equals(_month) && _temp.Year.Equals(_year))
+                    return _temp;
+
+            return null;
+        }
+        public static Sheet Show_sheet(int _month, int _year)
+        {
+            double _sum = 0;
+            foreach (Sheet _temp in _sheet)
+            {
+                if (_temp.Month.Equals(_month) && _temp.Year.Equals(_year))
+                {
+                    _sum += Sheet.Calc_liquid(_temp.Hour, _temp.Hour_value);
+                    /*
+                     * sei lá o que fazer aqui, se não tem ninguem na lista de employee ele retorna um null quando usa o método Search employee
+                     * na minha cabeça nao era pra crashar, mas eu recebo um nullreference exeption.
+                    */
+                    Console.Write($"Nome:{Notebook.Search_employee(_temp.Cpf).Name}\n" +
+                        $"Salário:{Sheet.Calc_liquid(_temp.Hour, _temp.Hour_value)}");
+                }
+            }
+            Console.WriteLine($"\nSalários totais do mes: R${_sum}");
+
+            return null;
+        }
+        public static List<Sheet> GetFolha() => _sheet;
+    }
+}
