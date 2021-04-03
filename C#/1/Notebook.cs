b91ce2269ec78448eb0ed811d8d6bb8b7d35c8e6
coshbar <43322369+coshbar@users.noreply.github.com>
2021-04-03 20:52:26 -0300

    Add files via upload

---
+++ C#/1/Notebook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Notebook
+    {
+        static List<Contato> _notebook = new();
+
+        public static void AddContato(Contato _contato) => _notebook.Add(_contato);
+        public static List<Contato> GetContato() => _notebook;
+        public static Contato SearchContato(string _email)
+        {
+            foreach(Contato _temp in _notebook)
+                if (_temp.Email.Equals(_email))
+                    return _temp;
+ 
+            return null;
+        }
+    }
+}
---
+++ C#/1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _option;
+            bool _hasData = false;
+
+            while (true)
+            {
+                Console.WriteLine("1 - Cadastro de funcionários\n2 - Cadastro da folha\n3 - Consultar folha\n4 - Listar folhas\n5 - Sair");
+                _option = int.Parse(Console.ReadLine());
+
+                switch (_option)
+                {
+                    case 1:
+                        Contato _contato = new();
+
+                        Console.WriteLine("CPF:");
+                        _contato.Cpf = Console.ReadLine();
+                        if (Notebook.SearchContato(_contato.Cpf) == null)
+                        {
+                            Console.WriteLine("Nome:");
+                            _contato.Name = Console.ReadLine();
+
+                            Notebook.AddContato(_contato);
+                            _hasData = true;
+                        }
+                        else
+                            Console.WriteLine("CPF já cadastrado");
+
+                        break;
+                    case 2:
+                        Contato _c = Notebook.SearchContato(Console.ReadLine());
+                        if (_c == null)
+                            Console.WriteLine("Contato nao existe");
+                        else
+                        {
+
+
+                        }
+                            
+                        break;
+                    case 3:
+
+                        if(_c == null)
+                            Console.WriteLine("Contato nao existe");
+                        else
+                            Console.WriteLine($"Nome:{ _c.Cpf}\nPhone:{_c.Name}\nEmail:{_c.Email}\n");
+                        break;
+                    case 4:
+                        
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opcao invalida.");
+                        break;
+                }
+            }
+        }
+    }
+}
